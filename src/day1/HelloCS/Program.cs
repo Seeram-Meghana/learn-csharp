8e5739277a6ef55c4a6bbf31b4889bb6ebdadb65
Seeram-Meghana <seermeghan7@gmail.com>
2024-07-16 19:47:30 +0530

    Meghana/16june work (#1)
    * initial checkin
    
    * final checkin

---
+++ src/day1/HelloCS/Program.cs
@@ -0,0 +1,15 @@
+﻿ForegroundColor = ConsoleColor.DarkCyan;
+ 
+WriteLine("Hello, C# 12, and DotNet 8 !");
+ 
+WriteLine("We received {0} arguments:", args.Length);
+foreach (string arg in args)
+{
+    WriteLine(arg);
+}
+ 
+WriteLine("CurrentDirectory: {0}", Env.CurrentDirectory);
+WriteLine("OSVersion.VersionString: {0}", Env.OSVersion.VersionString);
+WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "None!");
+ 
+ResetColor();
