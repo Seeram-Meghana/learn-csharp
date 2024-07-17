4a062304007b3d412ae68e0a866f8ae176008323
Seeram-Meghana <seermeghan7@gmail.com>
2024-07-17 20:51:29 +0530

    Meghana/16june work (#2)
    * initial checkin
    
    * final checkin
    
    * finial checkin

--- src/day1/HelloCS/Program.cs
+++ src/day1/HelloCS/Program.cs
@@ -1,13 +1,24 @@
 ﻿ForegroundColor = ConsoleColor.DarkCyan;
+
+
+WriteLine("Hello, C# 12, and DotNet 8 !");
+
+
  
 WriteLine("Hello, C# 12, and DotNet 8 !");
- 
+
 WriteLine("We received {0} arguments:", args.Length);
 foreach (string arg in args)
 {
     WriteLine(arg);
 }
- 
+
+
+WriteLine("CurrentDirectory: {0}", Env.CurrentDirectory);
+WriteLine("OSVersion.VersionString: {0}", Env.OSVersion.VersionString);
+WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "None!");
+
+
 WriteLine("CurrentDirectory: {0}", Env.CurrentDirectory);
 WriteLine("OSVersion.VersionString: {0}", Env.OSVersion.VersionString);
 WriteLine("Namespace: {0}", typeof(Program).Namespace ?? "None!");
