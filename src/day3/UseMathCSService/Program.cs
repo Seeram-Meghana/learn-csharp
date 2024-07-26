01d7532f8c86c8d566261a754fcfbb376ab84eec
Seeram-Meghana <seermeghan7@gmail.com>
2024-07-26 12:21:20 +0530

    finial checkin

---
+++ src/day3/UseMathCSService/Program.cs
@@ -0,0 +1,23 @@
+﻿using UseMathCSService.Services;
+
+ForegroundColor = ConsoleColor.Green;
+
+WriteLine("Hello .NET 8 World!");
+
+Write("Enter your name: ");
+var name = ReadLine();
+WriteLine($"Hello {name}!");
+
+MathsSvc maths = new();
+
+int num1 = 10, num2 = 20;
+WriteLine($"\n{num1} + {num2} = {maths.AddNumbers(num1, num2)}");
+
+WriteLine($"{num1} * {num2} = {maths.MultiplyNumbers(num1, num2)}");
+
+WriteLine($"Max value is {MathsSvc.MaxValue}");
+
+ResetColor();
+
+WriteLine("Press any key to exit.");
+ReadKey();
