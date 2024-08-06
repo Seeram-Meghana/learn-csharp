2126e46e1c7c7eadb022a0d4df5c743f894dffdd
Seeram-Meghana <seermeghan7@gmail.com>
2024-08-06 18:08:34 +0530

    final checkin

---
+++ src/day6/MathServicesCSLib/MathsCSSvc.cs
@@ -0,0 +1,13 @@
+﻿namespace MathServicesCSLib;
+
+public class MathsCSSvc
+{
+    public const int MaxValue = 100;
+
+    public int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+
+    public int MultiplyNumbers(int a, int b) => a * b;
+}
---
+++ src/day6/MathServicesVBLib/MathsVBSvc.vb
@@ -0,0 +1,13 @@
+﻿Namespace MathLibrary
+    Public Class MathsVBSvc
+        Public Shared MaxValue As Integer = 100
+
+        Public Function AddNumbers(a As Integer, b As Integer) As Integer
+            Return a + b
+        End Function
+
+        Public Function MultiplyNumbers(a As Integer, b As Integer) As Integer
+            Return a * b
+        End Function
+    End Class
+End Namespace
